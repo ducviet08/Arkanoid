8017f9167186777a432981c5aafd8dc3ea5f8bb6
Duytus <duytu19042001@gmail.com>
2025-09-27 03:46:45 +0700

    duytus - lololo - 27/9/2025

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("lọ vương");
+        System.out.println(" t cần kim cương làm cái đéo gì thích t bê cả khay ");
     }
 }
