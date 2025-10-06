258315bebf1b9cd8db3e814bde961b80297d98ff
NguyenVanToan <24020324@vnu.edu.vn>
2025-10-07 00:18:25 +0700

    add and improve ShrinkPaddle

--- src/model/ShrinkPaddle.java
+++ src/model/ShrinkPaddle.java
@@ -1,4 +1,23 @@
+// Arkanoid/model/ExpandPaddlePowerUp.java
 package model;
 
-public class ShrinkPaddle {
+public class ShrinkPaddle extends PowerUp {
+    private static final double SHRINK_AMOUNT = 50; // Lượng mở rộng thêm cho paddle
+    private static final double BUFF_SPEED_OF_BALL = 0.75; // Tốc độ của bóng khi mở rộng paddle
+
+    public ShrinkPaddle(double x, double y, double width, double height, double speed, long duration) {
+        super(x, y, width, height, speed, duration);
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle, Ball ball) {
+        ball.setSpeed(ball.getSpeed() * BUFF_SPEED_OF_BALL);
+        paddle.setWidth(paddle.getWidth() - SHRINK_AMOUNT);
+    }
+
+    @Override
+    public void removeEffect(Paddle paddle, Ball ball) {
+        ball.setSpeed(ball.getSpeed() / BUFF_SPEED_OF_BALL);
+        paddle.setWidth(paddle.getWidth() - SHRINK_AMOUNT);
+    }
 }
