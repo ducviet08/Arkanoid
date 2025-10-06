2d959d43dea9b1107d33656fcf5e6fb5bb4f6acc
NguyenVanToan <24020324@vnu.edu.vn>
2025-10-07 00:17:13 +0700

    improve ExpandPaddle

--- src/model/ExpandPaddlePowerUp.java
+++ src/model/ExpandPaddlePowerUp.java
@@ -3,17 +3,22 @@ package model;
 
 public class ExpandPaddlePowerUp extends PowerUp {
     private static final double EXPAND_AMOUNT = 50; // Lượng mở rộng thêm cho paddle
+    private static final double BUFF_SPEED_OF_BALL = 1.25; // Tốc độ của bóng khi mở rộng paddle
     private static final double FALL_SPEED = 2; // Tốc độ rơi của power-up
 
-    public ExpandPaddlePowerUp(double x, double y, double width, double height, long duration) {
-        super(x, y, width, height, FALL_SPEED, duration);
+    public ExpandPaddlePowerUp(double x, double y, double width, double height, double speed, long duration) {
+        super(x, y, width, height, speed, duration);
     }
 
     @Override
-    public void applyEffect(Paddle paddle) {
+    public void applyEffect(Paddle paddle, Ball ball) {
+        ball.setSpeed(ball.getSpeed() * BUFF_SPEED_OF_BALL);
+        paddle.setWidth(paddle.getWidth() + EXPAND_AMOUNT);
     }
 
     @Override
-    public void removeEffect(Paddle paddle) {
+    public void removeEffect(Paddle paddle, Ball ball) {
+        ball.setSpeed(ball.getSpeed() / BUFF_SPEED_OF_BALL);
+        paddle.setWidth(paddle.getWidth() - EXPAND_AMOUNT);
     }
 }
