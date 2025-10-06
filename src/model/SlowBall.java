7e10635860cd7974b2cb19f203e8d0b891a21209
NguyenVanToan <24020324@vnu.edu.vn>
2025-10-07 00:20:54 +0700

    add and improve SlowBall

--- src/model/SlowBall.java
+++ src/model/SlowBall.java
@@ -1,4 +1,28 @@
+// Arkanoid/model/FastBallPowerUp.java
 package model;
 
-public class SlowBall {
+public class SlowBall extends PowerUp {
+    private static final double SPEED_MULTIPLIER = 0.5; // Tăng tốc bóng lên 1.5 lần
+    private static final double FALL_SPEED = 2; // Tốc độ rơi của power-up
+    private Ball gameBall; // Tham chiếu đến quả bóng chính của game
+
+    public SlowBall(double x, double y, double width, double height, double speed, long duration) {
+        super(x, y, width, height, speed, duration);
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle, Ball ball) {
+        ball.setSpeed(ball.getSpeed() * SPEED_MULTIPLIER);
+    }
+
+    @Override
+    public void removeEffect(Paddle paddle, Ball ball) {
+        ball.setSpeed(ball.getSpeed() / SPEED_MULTIPLIER);
+    }
+
+    // Setter để GameManager có thể thiết lập tham chiếu đến ball
+    public void setGameBall(Ball gameBall) {
+        this.gameBall = gameBall;
+    }
+
 }
