6e9f11af44344b906ea92ff5ad7fb502b1b0ba53
NguyenVanToan <24020324@vnu.edu.vn>
2025-10-07 00:15:03 +0700

    improve Paddle

--- src/model/Paddle.java
+++ src/model/Paddle.java
@@ -2,11 +2,12 @@
 package model;
 
 public class Paddle extends MovableObject {
+    // tính toán sau
     private double originalWidth; // Kích thước ban đầu của paddle
 
     public Paddle(double x, double y, double width, double height, double speed) {
         super(x, y, width, height, speed);
-        this.originalWidth = width;
+
     }
 
     public void moveLeft() {
@@ -23,14 +24,7 @@ public class Paddle extends MovableObject {
 
     @Override
     public void update() {
-    }
-
-    public void applyPowerUp(PowerUp powerUp) {
-        powerUp.applyEffect(this);
-    }
-
-    public void resetWidth() {
-        this.width = originalWidth;
+        super.update();
     }
 
     @Override
