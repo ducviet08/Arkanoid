f618b9b50b32ecfd1fa19b4511434b1a63ed2920
NguyenVanToan <24020324@vnu.edu.vn>
2025-10-30 17:40:07 +0700

    refactor(sound): replace AutoClip with MediaPlayer

--- src/controller/SoundManager.java
+++ src/controller/SoundManager.java
@@ -1,28 +1,51 @@
 package controller;
 
-import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 import java.net.URL;
+import java.util.Objects;
 
 public class SoundManager {
-    private static AudioClip loadSound(String path) {
-        URL resource = SoundManager.class.getResource(path);
-        if(resource == null) {
-            System.out.println("Sound file not found: " + path);
-            return null;
+    public static final String SOUND_CLICK = "/sounds/click.mp3";
+    public static final String SOUND_PADDLE_HIT = "/sounds/BounceWithPaddle_sound.mp3";
+    public static final String SOUND_NORMALBRICK_HIT = "/sounds/BounceWithNormalBrick_sound.mp3";
+    public static final String SOUND_STEELBRICK_HIT = "/sounds/BounceWithSteelBrick.mp3";
+    public static final String SOUND_POWERUP_GET = "/sounds/powerup_get.mp3";
+    public static final String SOUND_LOSE_LIFE = "/sounds/fall_out.mp3";
+    public static final String SOUND_LEVEL_COMPLETE = "/sounds/click.mp3";
+    public static final String SOUND_GAME_OVER = "/sounds/game_over_sound.mp3";
+    public static final String SOUND_WIN_GAME = "/sounds/win.mp3";
+    public static final String SOUND_GAME_START = "/sounds/start_sound.mp3";
+
+
+    // Lưu player hiện tại để có thể stop khi cần
+    private static MediaPlayer currentPlayer;
+    private static String typeCurrentPlayer;
+
+    public static void playSound(String path) {
+        if(Objects.equals(typeCurrentPlayer, SOUND_WIN_GAME) || Objects.equals(typeCurrentPlayer, SOUND_GAME_OVER)) {
+            stopCurrentSound(); // dừng trước khi phát cái mới
         }
 
-        return new AudioClip(resource.toString());
+        URL resource = SoundManager.class.getResource(path);
+        if (resource == null) {
+            System.out.println("Sound file not found: " + path);
+            return;
+        }
+
+        Media media = new Media(resource.toString());
+        currentPlayer = new MediaPlayer(media);
+        typeCurrentPlayer = path;
+        currentPlayer.play();
     }
 
-    public static final AudioClip CLICK = loadSound("sounds/click.mp3");
-    public static final AudioClip PADDLE_HIT = loadSound("sounds/click.mp3");
-    public static final AudioClip BRICK_HIT = loadSound("sounds/click.mp3");
-    public static final AudioClip POWERUP_GET = loadSound("sounds/click.mp3");
-    public static final AudioClip LOSE_LIFE = loadSound("sounds/click.mp3");
-    public static final AudioClip LEVEL_COMPELTE = loadSound("sounds/click.mp3");
-    public static final AudioClip GAME_OVER = loadSound("sounds/click.mp3");
-
-    public static void play(AudioClip clip) {
-        if(clip != null) clip.play();
+    /** Hàm dùng để ngắt âm thanh  */
+    public static void stopCurrentSound() {
+        if (currentPlayer != null) {
+            currentPlayer.stop();
+            currentPlayer.dispose();
+            currentPlayer = null;
+        }
     }
 }
